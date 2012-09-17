e461742c6c36c4c00d2f5892b035766ac5a516a6
Abu Zahed Jony <jony.cse@hotmail.com>
2012-09-17 06:03:50 +0600

    singleton design pattern sample add

--- singleTone.py
+++ singleTone.py
@@ -1,15 +1,33 @@
-class Singleton:
-    __single = None
-    def __init__( self ):
-        if Singleton.__single:
-            raise Singleton.__single
-        Singleton.__single = self
+'''
+For implementing single tone we need to override the "__new__" method of class
+'''
 
+class SingletonObject(object):
+    _instance = None
 
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(SingletonObject, cls).__new__(
+                cls, *args, **kwargs)
+        return cls._instance
 
-class Child( Singleton ):
-    def __init__( self, name ):
-        Singleton.__init__( self )
-        self.__name = name
-    def name( self ):
+    def set_name(self,name):
+        self.__name=name # name is private
+    def get_name(self):
         return self.__name
+
+    def set_home(self,home):
+        self.home=home # home is public
+
+
+if __name__ == '__main__':
+    ob1=SingletonObject()
+    ob1.set_name("test name")
+    ob2=SingletonObject()
+    ob2.set_home("BD")
+
+    print ob1.get_name()
+    print ob2.get_name()
+
+    print ob1.home
+    print ob2.home
