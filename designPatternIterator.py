6c53d8b92d77e178dc323a0566fd663a45d997e9
Abu Zahed Jony <jony.cse@hotmail.com>
2012-10-07 09:19:11 +0600

    deisgn pattern zip

--- designPatternIterator.py
+++ designPatternIterator.py
@@ -1,1 +1,8 @@
-__author__ = 'user'
+def show_chars(n):
+    list=['A','B','C','D','E','F','G','H','I','J']
+    for data, pos in zip(list, range(n)):
+        yield data
+
+chars_set= show_chars(5)
+for ch in chars_set:
+    print ch
