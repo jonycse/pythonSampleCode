3040c0bb6c47624e27252909fe80537232b28d7d
Abu Zahed Jony <jony.cse@hotmail.com>
2013-01-15 08:45:42 +0600

    python getter setter

--- pythonGetterSetterAsProperty.py
+++ pythonGetterSetterAsProperty.py
@@ -1,4 +1,4 @@
-class MyClass():
+class MyClass(object):
     def __init__(self):
         self.__name = "" # define private variable
         self.__id = 0 # define private variable
@@ -12,15 +12,14 @@ class MyClass():
     def getId(self):
         return self.__id
 
+    idGS = property(getId, setId) # declare getter setter property for id
+
 
 
 a = MyClass()
 a.setName("Python")
-a.setId(1)
-
+a.idGS=5
 
 print "Name ", a.getName()
-print "Id ", a.getId()
+print "Id ", a.idGS
 
-#print a.__name #error
-#print a.__id #error
