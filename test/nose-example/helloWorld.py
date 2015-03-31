4b4aa8f0336cdd863fe97273d45966d2b24795d8
Abu Zahed Jony <jony.cse@hotmail.com>
2015-03-31 09:11:28 +0600

    nose example 1 added

---
+++ test/nose-example/helloWorld.py
@@ -0,0 +1,9 @@
+__author__ = 'AbuZahedJony'
+
+
+def add_two_num(a, b):
+    return a+b
+
+
+def multi_two_num(a, b):
+    return a*b
---
+++ test/nose-example/testHelloWorld.py
@@ -0,0 +1,39 @@
+from nose import with_setup
+
+__author__ = 'AbuZahedJony'
+
+'''
+
+nosetests testHelloWorld.py
+nosetests -s testHelloWorld.py
+'''
+
+from helloWorld import add_two_num
+from helloWorld import multi_two_num
+
+
+def m_setup():
+    print "\nRun SETUP"
+
+
+def m_teardown():
+    print "Run TEAR-DOWN"
+
+
+
+@with_setup(m_setup, m_teardown)
+def test_add_num():
+    print "Running test ADD"
+    assert add_two_num(2, 3) == 5
+    assert add_two_num(-2, 3) == 1
+    assert add_two_num(-2, -3) == -5
+
+
+
+@with_setup(m_setup, m_teardown)
+def test_multi_num():
+    print "Running test MULTI"
+    assert multi_two_num(2, 3) == 6
+    assert multi_two_num(-2, 3) == -6
+    assert multi_two_num(-2, -3) == 6
+
